68b1a8ba8de48752b6d2a6e08dcfc6ecf746f2d6
root <root@RaulTifrea>
2024-08-19 13:39:28 +0200

    added mail checker

--- mailChecker.py
+++ mailChecker.py
@@ -1,9 +1,10 @@
 from playwright.sync_api import sync_playwright
 
-storage_path = 'playwright/.auth/storage_state.json'
+#storage_path = 'playwright/.auth/storage_state.json'
+storage_path = '/root/PlayWrightPython/playwright/.auth/storage_state.json'
 
 with sync_playwright() as playwright:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch()
     context = browser.new_context(storage_state=storage_path)
     page = context.new_page()
     page.goto('https://gmail.com')
@@ -13,22 +14,20 @@ with sync_playwright() as playwright:
     emails = page.locator('div.UI table tr') # all email entries
     
     for email in emails.all():
-        is_new_email = email.locator("td li[data-tooltip='Mark as read']").count() == 1
-
-        if is_new_email:
+        if email.locator("td li[data-tooltip='Mark as read']").count() == 1:
             sender_name = email.locator('td span[email]:visible').inner_text()
             email_title = email.locator('td span[data-thread-id]:visible').inner_text()
-
-        new_emails.append([sender_name, email_title])
+            new_emails.append([sender_name, email_title])
     
     if len(new_emails) == 0:
         print("No new emails !")
     else:
         print(f"{len(new_emails)} new emails!")
-        print('-'*20)
+        print('-'*30)
 
-        for new_email in new_emails:
-            print(new_emails[0], new_email[0])
-            print('-'*20)
+        for index, new_email in enumerate(new_emails):
+            print(f'Sender: {new_email[0]}')
+            print(f'Subject: {new_email[1]}')
+            print('-'*30)
     
     context.close()
